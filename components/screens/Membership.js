3f547143eeb5df5341a91a50d3f343ad76a19e8c
unknown <harshaasodariya9469@gmail.com>
2022-02-25 22:21:50 +0530

    avani first commit

--- components/screens/Membership.js
+++ components/screens/Membership.js
@@ -7,6 +7,7 @@ import {
   Text,
   TouchableOpacity,
   View,
+  StyleSheet,
 } from 'react-native';
 
 import {
@@ -19,16 +20,17 @@ import {
   Checkbox,
   IconBtn,
 } from '../utils';
+import Slider from 'react-native-slider';
 import AntDesignIcon from 'react-native-vector-icons/dist/AntDesign';
-import {scale} from 'react-native-size-matters';
+import { scale } from 'react-native-size-matters';
 import EntypoIcon from 'react-native-vector-icons/Entypo';
-import {Colors} from '../Constant';
+import { Colors } from '../Constant';
 import Carousel from 'react-native-snap-carousel';
-import {Pagination} from 'react-native-snap-carousel';
+import { Pagination } from 'react-native-snap-carousel';
 
 import config from '../../config';
 import style from '../../style';
-const {width, height} = Dimensions.get('window');
+const { width, height } = Dimensions.get('window');
 import sample_data from '../../sample_data';
 export default class MemberShip extends React.Component {
   constructor() {
@@ -66,13 +68,20 @@ export default class MemberShip extends React.Component {
           ans:
             'There are many different ways to organize a paragraph. The organization you choose will depend on the controlling idea of the paragraph.',
         },
+        {
+          id: 6,
+          question: 'How do I organize a paragraph?',
+          ans:
+            'There are many different ways to organize a paragraph. The organization you choose will depend on the controlling idea of the paragraph.',
+        },
       ],
       currentIndex: 0,
       activeSlide1: 0,
+      value: 1000,
     };
   }
 
-  _renderCarouselItem = ({item, index}) => {
+  _renderCarouselItem = ({ item, index }) => {
     return (
       <View>
         <Text
@@ -121,12 +130,12 @@ export default class MemberShip extends React.Component {
   };
   render() {
     return (
-      <View style={{backgroundColor:"rgba(255,255,255,0.7)",}}>
+      <View style={{ backgroundColor: "rgba(255,255,255,0.7)", }}>
         <ScrollView bounces={false}  >
           <View>
             <Image
               source={require('../../assets/img/membershipBg.png')}
-              style={{height: scale(520), width: scale(350)}}
+              style={{ height: scale(520), width: scale(350) }}
             />
             <View
               style={{
@@ -141,7 +150,7 @@ export default class MemberShip extends React.Component {
                   justifyContent: 'space-between',
                   alignItems: 'center',
                 }}>
-                <View style={{flexDirection: 'row'}}>
+                <View style={{ flexDirection: 'row' }}>
                   <Image
                     source={require('../../assets/img/vita1.png')}
                     style={{
@@ -153,11 +162,11 @@ export default class MemberShip extends React.Component {
                   />
                   <Image
                     source={require('../../assets/img/vita2.png')}
-                    style={{width: scale(100), height: scale(30)}}
+                    style={{ width: scale(100), height: scale(30) }}
                     resizeMode="cover"
                   />
                 </View>
-                <View
+                <TouchableOpacity onPress={() => { this.props.navigation.navigate('Home') }}
                   style={{
                     backgroundColor: 'rgba(255,255,255,0.5)',
                     borderRadius: scale(10),
@@ -169,7 +178,7 @@ export default class MemberShip extends React.Component {
                     size={scale(13)}
                     color={Colors.primary}
                   />
-                </View>
+                </TouchableOpacity>
               </View>
               <View
                 style={{
@@ -207,11 +216,11 @@ export default class MemberShip extends React.Component {
                   borderRadius: scale(5),
                   justifyContent: 'center',
                   alignItems: 'flex-start',
-                  padding: scale(15),   shadowColor: '#171717',
-                  shadowOffset: {width: -2, height: 4},
+                  padding: scale(15), shadowColor: '#171717',
+                  shadowOffset: { width: -2, height: 4 },
                   shadowOpacity: 0.2,
                   shadowRadius: 3,
-                  
+
                 }}>
                 <View
                   style={{
@@ -229,7 +238,7 @@ export default class MemberShip extends React.Component {
                       resizeMode: 'contain',
                     }}
                   />
-                  <View style={{width: '80%'}}>
+                  <View style={{ width: '80%' }}>
                     <Text
                       style={{
                         color: '#000',
@@ -264,7 +273,7 @@ export default class MemberShip extends React.Component {
                       resizeMode: 'contain',
                     }}
                   />
-                  <View style={{width: '80%'}}>
+                  <View style={{ width: '80%' }}>
                     <Text
                       style={{
                         color: '#000',
@@ -302,7 +311,7 @@ export default class MemberShip extends React.Component {
                       resizeMode: 'contain',
                     }}
                   />
-                  <View style={{width: '80%'}}>
+                  <View style={{ width: '80%' }}>
                     <Text
                       style={{
                         color: '#000',
@@ -321,7 +330,7 @@ export default class MemberShip extends React.Component {
                     </Text>
                   </View>
                 </View>
-                <View style={{marginTop: scale(15)}}>
+                <View style={{ marginTop: scale(15) }}>
                   <Text
                     style={{
                       color: '#606060',
@@ -394,9 +403,9 @@ export default class MemberShip extends React.Component {
           </View>
           <View
             style={{
-               paddingHorizontal:scale(15)
+              paddingHorizontal: scale(15)
             }}>
-            <View style={{width: '100%'}}>
+            <View style={{ width: '100%' }}>
               <Text
                 style={{
                   fontSize: scale(15),
@@ -408,7 +417,7 @@ export default class MemberShip extends React.Component {
 
               <View
                 style={{
-                  width: '100%', 
+                  width: '100%',
                 }}>
                 <View
                   style={{
@@ -420,10 +429,10 @@ export default class MemberShip extends React.Component {
                     justifyContent: 'space-between',
                     paddingHorizontal: scale(5),
                     marginVertical: scale(5),
-                    borderRadius:scale(5)
+                    borderRadius: scale(5)
                   }}>
                   <Checkbox style />
-                  <Text style={{paddingHorizontal: scale(5)}}>3 Months</Text>
+                  <Text style={{ paddingHorizontal: scale(5) }}>3 Months</Text>
                   <Text
                     style={{
                       textDecorationLine: 'line-through',
@@ -465,10 +474,10 @@ export default class MemberShip extends React.Component {
                     justifyContent: 'space-between',
                     paddingHorizontal: scale(5),
                     marginVertical: scale(5),
-                    borderRadius:scale(5)
+                    borderRadius: scale(5)
                   }}>
                   <Checkbox style />
-                  <Text style={{paddingHorizontal: scale(5)}}>6 Months</Text>
+                  <Text style={{ paddingHorizontal: scale(5) }}>6 Months</Text>
                   <Text
                     style={{
                       textDecorationLine: 'line-through',
@@ -502,13 +511,56 @@ export default class MemberShip extends React.Component {
                 </View>
               </View>
             </View>
+            <View style={[styles.shadowProp, styles.medicinecard]}>
+              <View style={[styles.cardtopconent]}>
+                <Text style={[styles.cardToptextsml]}>As a Plus member you will save upto</Text>
+                <Text style={[styles.cardtextpay]}>₹2000</Text>
+                <Text style={[styles.cardsubtext]}>if your spending on medicine is</Text>
+                <View  style={[styles.cardslider]}>
+                  <Slider
+                    style={{ height: scale(20), width: scale(280) }}
+                    trackStyle={customStyles6.track}
+                    thumbStyle={customStyles6.thumb}
+                    minimumValue={1000}
+                    maximumValue={6000}
+                    step={1000}
+                    thumbTouchSize={{ width: 50, height: 50 }}
+                    minimumTrackTintColor={Colors.primary}
+                    value={this.state.value}
+                    onValueChange={(value) => this.setState({ value })}
+                  />
+                  <Text style={[styles.cardslidervalue]} >₹ {this.state.value} / Month</Text>
+                </View>
+              </View>
+              <View style={[styles.cardtbtmconent]}>
+                <Text style={[styles.cardclicktext]}>
+                  Click here&nbsp;
+                  <Text>to know more</Text>
+                </Text>
+              </View>
+            </View>
+            {/* <View>
+           <Slider
+          style={{ height: scale(20), width: scale(320)}}
+          trackStyle={customStyles6.track}
+          thumbStyle={customStyles6.thumb}
+          minimumValue={1000}
+          maximumValue={6000}
+          step={1000}
+          thumbTouchSize={{width:50,height:50}}
+          minimumTrackTintColor={Colors.primary}
+          value={this.state.value}
+          onValueChange={(value) => this.setState({value})}
+        />
+        <Text>Value: {this.state.value}</Text>
+           </View> */}
             <View
               style={{
                 justifyContent: 'center',
                 alignItems: 'center',
                 paddingVertical: scale(5),
               }}>
-              <View style={{width: '100%'}}>
+              <View style={{ width: '100%' }}>
                 <Text
                   style={{
                     fontSize: scale(15),
@@ -532,7 +584,7 @@ export default class MemberShip extends React.Component {
                     sliderWidth={width - 30}
                     itemWidth={width - 30}
                     onSnapToItem={(index) =>
-                      this.setState({activeSlide1: index})
+                      this.setState({ activeSlide1: index })
                     }
                   />
                   <Pagination
@@ -557,6 +609,7 @@ export default class MemberShip extends React.Component {
                   />
                 </View>
               </View>
+
             </View>
 
             <View
@@ -564,7 +617,7 @@ export default class MemberShip extends React.Component {
                 justifyContent: 'center',
                 alignItems: 'center',
               }}>
-              <View style={{width: '100%'}}>
+              <View style={{ width: '100%' }}>
                 <Text
                   style={{
                     fontSize: scale(15),
@@ -577,7 +630,7 @@ export default class MemberShip extends React.Component {
               <FlatList
                 data={this.state.QA}
                 //   nestedScrollEnabled
-                renderItem={({item, index}) => {
+                renderItem={({ item, index }) => {
                   return (
                     <TouchableOpacity
                       style={{
@@ -593,11 +646,11 @@ export default class MemberShip extends React.Component {
                           this.state.currentIndex == 0 ||
                           this.state.currentIndex !== index + 1
                         )
-                          this.setState({currentIndex: index + 1}, () => {
+                          this.setState({ currentIndex: index + 1 }, () => {
                             console.log(this.state.currentIndex);
                           });
                         else
-                          this.setState({currentIndex: 0}, () => {
+                          this.setState({ currentIndex: 0 }, () => {
                             console.log(this.state.currentIndex);
                           });
                       }}>
@@ -610,7 +663,7 @@ export default class MemberShip extends React.Component {
                         <EntypoIcon
                           name="chevron-down"
                           color={Colors.primary}
-                          style={{paddingHorizontal: scale(5)}}
+                          style={{ paddingHorizontal: scale(5) }}
                           size={scale(15)}
                         />
                       </View>
@@ -630,17 +683,18 @@ export default class MemberShip extends React.Component {
                 width: scale(320),
               }}
             />
-            <View style={{justifyContent: 'center', alignItems: 'center'}}>
-              <Text style={{color: Colors.darkGray, fontSize: scale(12)}}>
+            <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+              <Text style={{ color: Colors.darkGray, fontSize: scale(12) }}>
                 {'Terms & Conditions'}
               </Text>
             </View>
           </View>
-          <View style={{paddingBottom: scale(100)}} />
+
+          <View style={{ paddingBottom: scale(100) }} />
         </ScrollView>
 
-     
-    
+
+
         <View
           style={{
             position: 'absolute',
@@ -661,7 +715,7 @@ export default class MemberShip extends React.Component {
               alignItems: 'center',
               borderRadius: scale(5),
             }}>
-            <Text style={{color: Colors.white, fontSize: scale(12)}}>
+            <Text style={{ color: Colors.white, fontSize: scale(12) }}>
               {'Get Plus'}
             </Text>
           </TouchableOpacity>
@@ -670,3 +724,101 @@ export default class MemberShip extends React.Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  medicinecard: {
+    borderRadius: 6,
+    textAlign: "center",
+    marginTop: 15,
+    marginBottom: 15,
+  },
+  cardToptextsml: {
+    color: '#515A60',
+    fontSize: 12,
+    textAlign: "center",
+  },
+  cardslider:{
+    marginVertical: 20,
+  },
+  cardslidervalue:{
+    textAlign: "center",
+    color: '#434c53',
+    fontSize: 14,
+    marginTop: 15,
+  },
+  cardtextpay: {
+    color: '#4F585E',
+    textAlign: "center",
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginVertical: 5,
+  },
+  cardsubtext: {
+    color: '#84939F',
+    fontSize: 12,
+    textAlign: "center",
+  },
+  cardtopconent: {
+    padding: 15
+  },
+  cardtbtmconent: {
+    padding: 8,
+    backgroundColor: "#F8F8FE",
+    textAlign: "center",
+  },
+  cardclicktext: {
+    textAlign: "center",
+    color: '#535C62',
+  },
+  // logo: {
+  //   height: 80
+  // },
+  shadowProp: {
+    shadowColor: "#171717",
+    shadowOffset: { width: 15, height: 15 },
+    shadowOpacity: 0.20,
+    shadowRadius: 3,
+    elevation: 1,
+    // shadowColor: "#000",
+    // shadowOffset: {
+    //   width: 0,
+    //   height: 9,
+    // },
+    // shadowOpacity: 0.48,
+    // shadowRadius: 11.95,
+    // elevation: 1,
+    // shadowOffset: {
+    //   width: 8,
+    //   height: 8,
+    // },
+    // shadowOpacity: 0.90,
+    // shadowRadius: 10,
+    // elevation: 5,
+    // shadowOpacity: 0.01,
+
+  },
+})
+
+var customStyles6 = StyleSheet.create({
+  track: {
+    height: scale(8),
+    borderRadius: 10,
+    backgroundColor: 'rgba(229,171,193,0.15)',
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 1 },
+    shadowRadius: 1,
+    shadowOpacity: 0.15,
+  },
+  thumb: {
+    width: scale(25),
+    height: scale(25),
+    backgroundColor: 'gray',
+    borderColor: 'gray',
+    borderWidth: 5,
+    borderRadius: 15,
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 2,
+    shadowOpacity: 0.35,
+  }
+});
